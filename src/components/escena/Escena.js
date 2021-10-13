b18f105289ad1088f37a827599d4eaa172d6c5c9
silviafado <silviafado@gmail.com>
2021-10-13 18:23:15 +0200

    Exercise1

---
+++ src/components/escena/Escena.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const MyComponent1 = _ =>
+    <div>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</div>
+
+
+export default MyComponent1;
